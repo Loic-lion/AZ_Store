b4690aa2308c6a8118fccf6265fa842f914bf94a
Loic Lion <loic.lion.contact@gmail.com>
2023-06-08 11:30:02 +0200

    fichier php header+footer

---
+++ display.php
@@ -0,0 +1,27 @@
+<?php
+echo '<header>
+<h1>AZ[store]</h1>
+<nav> 
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="index.php">About</a></li>
+  <li><a href="index.php">Product</a></li>
+  <li><a href="index.php">Contact</a></li>
+  <li><a href="shopping-cart.php"><img src="./assets/img/shopping-cart.svg">Login</a><li>
+</ul>
+</nav>
+</header>
+
+
+
+<footer>
+<nav>
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="index.php">About</a></li>
+<li><a href="index.php">Product</a></li>
+<li><a href="index.php">Contact</a></li>
+</ul>
+</nav>
+</footer>'
+?>;
--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>AZ Store</title>
 </head>
+
 <body>
-    
+    <?php include 'display.php';    ?>
+    <main>
+        <section class="container_leading">
+            
+        </section>
+        <section class="container_store"></section>
+        <section class="container_pub">
+            <img src="./assets/img/shoe_two.png">
+            <h2> WE PROVIDE YOU THE BEST QUALITY. </h2>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ratione eos consequuntur facilis voluptatum doloremque, officia nobis libero itaque, voluptas, beatae blanditiis! </p>
+        </section>
+        <section class="container_opinion">
+            <div class="container_opinion_cellule">
+                <img src="./assets/img/image-emily.jpg">
+                <h3>Emily from xyz </h3>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quam labore soluta eum optio. </p>
+            </div>
+            <img src="./assets/img/image-thomas.jpg">
+            <h3>Thomas from corporate </h3>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quam labore soluta eum optio. </p>
+            </div>
+            <img src="./assets/img/image-jennie.jpg">
+            <h3>Jennie from Nike </h3>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quam labore soluta eum optio. </p>
+            </div>
+        </section>
+    </main>
 </body>
+
 </html>
