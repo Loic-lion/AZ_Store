8554ba6265970b7eade28209c67cf23957725e11
Loic-lion <loic.lion.contact@gmail.com>
2023-06-09 15:26:08 +0200

    Add files via upload

--- checkout.php
+++ checkout.php
@@ -5,151 +5,157 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="./assets/scss/css/main.css" />
     <title>Checkout</title>
 </head>
+
 <body>
- <?php include 'display-header.php'    ?>
-<div class="container">
-    <form method="GET" action="checkout.php">
-       <p class="form__field">
-           <label for="firstname" class="form__label">Enter your firstname :</label>
-           <input id="firstname" name="firstname" type="text" class="form__input" required>
-       </p>
-       <p class="form__field">
-           <label for="lastname" class="form__label">Enter your lastname:</label>
-           <input id="lastname" name="lastname" type="text" class="form__input" required>
-       </p>
-       <p class="form__field">
-           <label for="email" class="form__label">Enter your email :</label>
-           <input id="email" name="email" type="email" class="form__input" required>
-       </p>
-       <p class="form__field">
-           <label for="address" class="form__label">Enter your address :</label>
-           <input id="address" name="address" type="text" class="form__input" required>
-       </p>
-       <p class="form__field">
-           <label for="city" class="form__label">Enter your city :</label>
-           <input id="city" name="city" type="text" class="form__input" required>
-       </p>
-       <p class="form__field">
-           <label for="zip-code" class="form__label">Enter your zip-code :</label>
-           <input id="zip-code" name="zip-code" type="number" class="form__input" required>
-       </p>
-       <p class="form__field">
-           <label for="country" class="form__label">Enter your country :</label>
-           <input id="country" name="country" type="text" class="form__input" required>
-       </p>
-       <p class="form__submit">
-           <input name="submit" value="Submit" type="submit" class="form__button" required>
-       </p>
-    </form>
-</div>
+
+    <?php include 'display-header.php'    ?>
+    <main>
+        <div class="container">
+            <form method="GET" action="checkout.php">
+                <p class="form__field">
+                    <label for="firstname" class="form__label">Enter your firstname :</label>
+                    <input id="firstname" name="firstname" type="text" class="form__input" required>
+                </p>
+                <p class="form__field">
+                    <label for="lastname" class="form__label">Enter your lastname:</label>
+                    <input id="lastname" name="lastname" type="text" class="form__input" required>
+                </p>
+                <p class="form__field">
+                    <label for="email" class="form__label">Enter your email :</label>
+                    <input id="email" name="email" type="email" class="form__input" required>
+                </p>
+                <p class="form__field">
+                    <label for="address" class="form__label">Enter your address :</label>
+                    <input id="address" name="address" type="text" class="form__input" required>
+                </p>
+                <p class="form__field">
+                    <label for="city" class="form__label">Enter your city :</label>
+                    <input id="city" name="city" type="text" class="form__input" required>
+                </p>
+                <p class="form__field">
+                    <label for="zip-code" class="form__label">Enter your zip-code :</label>
+                    <input id="zip-code" name="zip-code" type="number" class="form__input" required>
+                </p>
+                <p class="form__field">
+                    <label for="country" class="form__label">Enter your country :</label>
+                    <input id="country" name="country" type="text" class="form__input" required>
+                </p>
+                <p class="form__submit">
+                    <input name="submit" value="Submit" type="submit" class="form__button" required>
+                </p>
+            </form>
+        </div>
 
 
-<?php
-if(isset($_GET['submit'])) {
-    $firstname = $_GET['firstname'];
-    $lastname = $_GET['lastname'];
-    $email = $_GET['email'];
-    $address = $_GET['address'];
-    $city = $_GET['city'];
-    $zipCode = $_GET['zip-code'];
-    $country = $_GET['country'];
+        <?php
+        if (isset($_GET['submit'])) {
+            $firstname = $_GET['firstname'];
+            $lastname = $_GET['lastname'];
+            $email = $_GET['email'];
+            $address = $_GET['address'];
+            $city = $_GET['city'];
+            $zipCode = $_GET['zip-code'];
+            $country = $_GET['country'];
 
-    $filterFirstname = filter_var($firstname, FILTER_SANITIZE_STRING);
-    $filterLastname = filter_var($lastname, FILTER_SANITIZE_STRING);
-    $filterEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
-    $filterAddress = filter_var($address, FILTER_SANITIZE_STRING);
-    $filterCity = filter_var($city, FILTER_SANITIZE_STRING);
-    $filterZipCode = filter_var($zipCode, FILTER_SANITIZE_NUMBER_INT);
-    $filterCountry = filter_var($country, FILTER_SANITIZE_STRING);
+            $filterFirstname = filter_var($firstname, FILTER_SANITIZE_STRING);
+            $filterLastname = filter_var($lastname, FILTER_SANITIZE_STRING);
+            $filterEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+            $filterAddress = filter_var($address, FILTER_SANITIZE_STRING);
+            $filterCity = filter_var($city, FILTER_SANITIZE_STRING);
+            $filterZipCode = filter_var($zipCode, FILTER_SANITIZE_NUMBER_INT);
+            $filterCountry = filter_var($country, FILTER_SANITIZE_STRING);
 
-    $errors = [];
+            $errors = [];
 
-    if(empty($filterFirstname)) {
-        $errors[] = 'Firstname is required';
-    }
-    if(empty($filterLastname)) {
-        $errors[] = 'Lastname is required';
-    }
-    if(empty($filterEmail)) {
-        $errors[] = 'Email is required';
-    }
-    if(empty($filterAddress)) {
-        $errors[] = 'The address is required';
-    }
-    if(empty($filterCity)) {
-        $errors[] = 'City is required';
-    }
-    if(empty($filterZipCode)) {
-        $errors[] = 'ZipCode is required';
-    }
-    if(empty($filterCountry)) {
-        $errors[] = 'Country is required';
-    }
+            if (empty($filterFirstname)) {
+                $errors[] = 'Firstname is required';
+            }
+            if (empty($filterLastname)) {
+                $errors[] = 'Lastname is required';
+            }
+            if (empty($filterEmail)) {
+                $errors[] = 'Email is required';
+            }
+            if (empty($filterAddress)) {
+                $errors[] = 'The address is required';
+            }
+            if (empty($filterCity)) {
+                $errors[] = 'City is required';
+            }
+            if (empty($filterZipCode)) {
+                $errors[] = 'ZipCode is required';
+            }
+            if (empty($filterCountry)) {
+                $errors[] = 'Country is required';
+            }
 
-    if(!empty($errors)) {
-        foreach($errors as $error) {
-            echo '<span class="error">'.$error.'</span>';
-        }
-    } else {
-        unset($_SESSION['shopping-cart']);
+            if (!empty($errors)) {
+                foreach ($errors as $error) {
+                    echo '<span class="error">' . $error . '</span>';
+                }
+            } else {
+                unset($_SESSION['shopping-cart']);
 
-        echo '
+                echo '
         <div class="result">
             <h2>Informations</h2>
-            <ul>
-                <li><strong>Firstname : </strong> <span class="result-user">' .$filterFirstname.'</span></li>
-                <li><strong>Lastname : </strong> <span class="result-user">' .$filterLastname.'</span></li>
-                <li><strong>Email : </strong> <span class="result-user">' .$filterEmail.'</span></li>
-                <li><strong>Address : </strong> <span class="result-user">' .$filterAddress.'</span></li>
-                <li><strong>City : </strong> <span class="result-user">' .$filterCity.'</span></li>
-                <li><strong>ZipCode :</strong> <span class="result-user">' .$filterZipCode.'</span></li>
-                <li><strong>Country :</strong> <span class="result-user">' .$filterCountry.'</span></li>
+            <ul class="result_info">
+                <li><strong>Firstname : </strong> <span class="result-user">' . $filterFirstname . '</span></li>
+                <li><strong>Lastname : </strong> <span class="result-user">' . $filterLastname . '</span></li>
+                <li><strong>Email : </strong> <span class="result-user">' . $filterEmail . '</span></li>
+                <li><strong>Address : </strong> <span class="result-user">' . $filterAddress . '</span></li>
+                <li><strong>City : </strong> <span class="result-user">' . $filterCity . '</span></li>
+                <li><strong>ZipCode :</strong> <span class="result-user">' . $filterZipCode . '</span></li>
+                <li><strong>Country :</strong> <span class="result-user">' . $filterCountry . '</span></li>
             </ul>
         </div>';
 
-        retrieveItems();
-    }
-}
-session_start();
+                retrieveItems();
+            }
+        }
+        session_start();
 
-function retrieveItems()
-{
-    $jsonFile = './assets/json/cart.json';
-    $data = file_get_contents($jsonFile);
-    $items = json_decode($data, true);
+        function retrieveItems()
+        {
+            $jsonFile = './assets/json/cart.json';
+            $data = file_get_contents($jsonFile);
+            $items = json_decode($data, true);
 
-    echo '<h2>Order</h2>';
-    echo '<ul>';
+            echo '<h2>Order</h2>';
+            echo '<ul result_card>';
 
-    foreach ($items as $item) {
-        echo '<li>';
-        echo '<h3>' . $item['product'] . '</h3>';
-        echo '<img src="' . $item['image_url'] . '">';
-        echo '<p>Prix: ' . $item['price'] . '</p>';
-        echo '<p>Quantité: ' . $item['number'] . '</p>';
-        echo '</li>';
-    }
+            foreach ($items as $item) {
+                echo '<li>';
+                echo '<h3>' . $item['product'] . '</h3>';
+                echo '<img class="result_card_photo" src="' . $item['image_url'] . '">';
+                echo '<p>Prix: ' . $item['price'] . '</p>';
+                echo '<p>Quantité: ' . $item['number'] . '</p>';
+                echo '</li>';
+            }
 
-    echo '</ul>';
-    $total = calculateTotal($items);
-    echo '<p>Total: ' . $total . ' euros</p><br>';
-    echo '<div class="display"><p>Thank you for your order</p></div>';
-    unset($_SESSION['shopping-cart']);
-}
+            echo '</ul>';
+            $total = calculateTotal($items);
+            echo '<p>Total: ' . $total . ' euros</p><br>';
+            echo '<div class="display"><p>Thank you for your order</p></div>';
+            unset($_SESSION['shopping-cart']);
+        }
 
-function calculateTotal($items)
-{
-    $total = 0;
+        function calculateTotal($items)
+        {
+            $total = 0;
 
-    foreach ($items as $item) {
-        $total += $item['price'];
-    }
-    return $total;
-}
-?>
+            foreach ($items as $item) {
+                $total += $item['price'];
+            }
+            return $total;
+        }
+        ?>
+    </main>
+    <?php include 'display-footer.php'    ?>
 </body>
-  <?php include 'display-footer.php'    ?>
+
+
 </html>
--- shopping-cart.php
+++ shopping-cart.php
@@ -5,142 +5,149 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./assets/scss/css/main.css" />
+    <link rel="stylesheet" href="./assets/scss/css/main.css" />
     <title>AZ Store</title>
 
 </head>
+
 <body>
-  <header>
+
     <?php include 'display-header.php'    ?>
-  </header>
-<main>
-<?php include 'display-header.php'    ?>
-  
-    <?php
-    $jsonFile = 'assets/json/cart.json';
 
-    function createCartItem($json, $id)
-    {
-        $shoppingCart = json_decode($json, true);
-        array_push($shoppingCart, $json[$id]);
-        saveShoppingCart($shoppingCart);
-        displayShoppingCart();
-        return $json;
-    }
+    <main>
 
-    function displayShoppingCart()
-    {
-        ob_clean();
-        global $jsonFile;
-        $json = file_get_contents($jsonFile);
-        $cart = json_decode($json, true);
-        $total = 0;
-        if ($cart != "") {
-            foreach ($cart as $index => $item) {
-                $price = $item["price"] * $item["number"];
-                echo "<div class='Cart__Item'>";
-                echo "<span class='Cart__Item__Product'>$item[product]</span>";
-                echo "<span class='Cart__Item__Price'>$price</span>";
-                echo "<img class='Cart__Item__Image' src='$item[image_url]' alt='Picture of a shoe'>";
-                echo '<form action="shopping-cart.php" method="post">';
-                echo "<input type='hidden' name='Substract_Number' value='$index'>";
-                echo "<input type='submit' class='Cart__Item__Substract' value='-'>";
-                echo "</form>";
-                echo "<span class='Cart__Item__Price'>$item[number]</span>";
-                echo '<form action="shopping-cart.php" method="post">';
-                echo "<input type='hidden' name='Add_Number' value='$index'>";
-                echo "<input type='submit' class='Cart__Item__Add' value='+'>";
-                echo '</form>';
-                echo "<form action='shopping-cart.php' method='post'>";
-                echo "<input type='hidden' name='remove_item' value='$index'>";
-                echo "<input type='submit' class='Cart__Item__Remove' value='Remove'>";
-                echo "</form>";
-                echo "</div>";
-                $total = $total + $price;
+        <?php
+        ob_start();
+        $jsonFile = 'assets/json/cart.json';
+
+        function createCartItem($json, $id)
+        {
+            $shoppingCart = json_decode($json, true);
+            array_push($shoppingCart, $json[$id]);
+            saveShoppingCart($shoppingCart);
+            displayShoppingCart();
+            return $json;
+        }
+
+        function displayShoppingCart()
+        {
+            ob_clean();
+            global $jsonFile;
+            $json = file_get_contents($jsonFile);
+            $cart = json_decode($json, true);
+            $total = 0;
+            echo '<section class="Cart">';
+            if ($cart != "") {
+                foreach ($cart as $index => $item) {
+                    $price = $item["price"] * $item["number"];
+                    echo "<div class='Cart__Item'>";
+                    echo "<img class='Cart__Item__Image' src='$item[image_url]' alt='Picture of a shoe'>";
+                    echo "<span class='Cart__Item__Product'>$item[product]</span>";
+                    echo "<span class='Cart__Item__Price'>$price € </span>";
+
+                    echo '<form action="shopping-cart.php" method="post">';
+                    echo "<input type='hidden' name='Substract_Number' value='$index'>";
+                    echo "<input type='submit' class='Cart__Item__Substract' value='-'>";
+                    echo "</form>";
+                    echo "<span class='Cart__Item__Price'>$item[number]</span>";
+                    echo '<form action="shopping-cart.php" method="post">';
+                    echo "<input type='hidden' name='Add_Number' value='$index'>";
+                    echo "<input type='submit' class='Cart__Item__Add' value='+'>";
+                    echo '</form>';
+                    echo "<form action='shopping-cart.php' method='post'>";
+                    echo "<input type='hidden' name='remove_item' value='$index'>";
+                    echo "<input type='submit' class='Cart__Item__Remove' value='Remove'>";
+                    echo "</form>";
+                    echo "</div>";
+
+                    $total = $total + $price;
+                }
+                echo "</section>";
+                if ($total != 0) {
+                    echo "<span class='Cart__Item__Total'>$total €</span>";
+                }
             }
-            if ($total != 0) {
-                echo "<span class='Cart__Item__Total'>$total</span>";
-            }
         }
-    }
 
-    function removeCartItem($index)
-    {
-        global $jsonFile;
-        $json = file_get_contents($jsonFile);
-        $cart = json_decode($json, true);
+        function removeCartItem($index)
+        {
+            global $jsonFile;
+            $json = file_get_contents($jsonFile);
+            $cart = json_decode($json, true);
 
-        if (isset($cart[$index])) {
-            unset($cart[$index]);
-            saveShoppingCart($cart);
-        }
-        displayShoppingCart();
-    }
-
-    function saveShoppingCart($cart)
-    {
-        global $jsonFile;
-        $json = json_encode($cart, JSON_PRETTY_PRINT);
-        file_put_contents($jsonFile, $json);
-    }
-
-    function addNumber($index)
-    {
-        global $jsonFile;
-        $json = file_get_contents($jsonFile);
-        $cart = json_decode($json, true);
-
-        if (isset($cart[$index])) {
-            $cart[$index]["number"] = $cart[$index]["number"] + 1;
-            saveShoppingCart($cart);
-        }
-        displayShoppingCart();
-    }
-    function substractNumber($index)
-    {
-        global $jsonFile;
-        $json = file_get_contents($jsonFile);
-        $cart = json_decode($json, true);
-        if ($cart[$index]["number"] > 1) {
             if (isset($cart[$index])) {
-                $cart[$index]["number"] = $cart[$index]["number"] - 1;
+                unset($cart[$index]);
                 saveShoppingCart($cart);
             }
             displayShoppingCart();
         }
-    }
+
+        function saveShoppingCart($cart)
+        {
+            global $jsonFile;
+            $json = json_encode($cart, JSON_PRETTY_PRINT);
+            file_put_contents($jsonFile, $json);
+        }
+
+        function addNumber($index)
+        {
+            global $jsonFile;
+            $json = file_get_contents($jsonFile);
+            $cart = json_decode($json, true);
+
+            if (isset($cart[$index])) {
+                $cart[$index]["number"] = $cart[$index]["number"] + 1;
+                saveShoppingCart($cart);
+            }
+            displayShoppingCart();
+        }
+        function substractNumber($index)
+        {
+            global $jsonFile;
+            $json = file_get_contents($jsonFile);
+            $cart = json_decode($json, true);
+            if ($cart[$index]["number"] > 1) {
+                if (isset($cart[$index])) {
+                    $cart[$index]["number"] = $cart[$index]["number"] - 1;
+                    saveShoppingCart($cart);
+                }
+                displayShoppingCart();
+            }
+        }
 
 
 
-    if (isset($_POST['submit'])) {
+        if (isset($_POST['submit'])) {
+            displayShoppingCart();
+        }
+
+        if (isset($_POST['remove_item'])) {
+            $itemIndex = $_POST['remove_item'];
+            removeCartItem($itemIndex);
+        }
         displayShoppingCart();
-    }
-
-    if (isset($_POST['remove_item'])) {
-        $itemIndex = $_POST['remove_item'];
-        removeCartItem($itemIndex);
-    }
-    displayShoppingCart();
 
 
 
-    if (isset($_POST['Add_Number'])) {
-        $itemIndex = $_POST['Add_Number'];
-        addNumber($itemIndex);
+        if (isset($_POST['Add_Number'])) {
+            $itemIndex = $_POST['Add_Number'];
+            addNumber($itemIndex);
+        }
+        if (isset($_POST['Substract_Number'])) {
+            $itemIndex = $_POST['Substract_Number'];
+            substractNumber($itemIndex);
+        }
 
 
-    }
-    if (isset($_POST['Substract_Number'])) {
-        $itemIndex = $_POST['Substract_Number'];
-        substractNumber($itemIndex);
-    }
-    ?>
-  
+        echo "<form action='checkout.php'>";
+        echo "<button class='Cart__Confirm'>Confirm</button>";
+        echo "</form>";
+        ?>
 
-</main>
-  <footer>
-     <?php include 'display-footer.php'   ?>
-  </footer>
+
+    </main>
+    <?php include 'display-footer.php'   ?>
+
 
 </body>
 
